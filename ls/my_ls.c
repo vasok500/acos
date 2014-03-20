4516c543021bceff87130abf6a18fc52c66d8ef3
vasya <vasya@vasya-HP.(none)>
2014-03-20 17:41:17 +0400

    Initial commit

---
+++ ls/my_ls.c
@@ -0,0 +1,153 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <pwd.h>
+int aflag,lflag;
+void GetKeys(int argc, char *argv[])
+{
+    int ch;
+    while ((ch = getopt(argc,argv,"al")) != -1)
+    {
+        switch (ch)
+        {
+            case 'a':
+                     aflag = 1;
+                     break;
+            case 'l':
+                     lflag = 1;
+                     break;
+            default:
+                     break;
+        }    
+    }
+}
+void List(char* path)
+{
+	int k = 0;
+    struct stat st;
+    if (path == NULL)
+    { 
+	path = (char*)malloc(2);
+	strcpy(path,".\0");
+	k = 1;	
+    }  
+    if (lstat(path,&st))
+    {
+        fprintf(stderr,"Error: path or file  does not exist.\n");
+	return;
+    }
+    if (S_ISDIR(st.st_mode))
+    {  
+        ListDir(path); 
+	if (k==1)
+	free(path);
+    }  
+    else if (S_ISREG(st.st_mode))
+    {  
+        Print(path); 
+    }  
+    else
+    {  
+        fprintf(stderr,"Not a regular file or directory.\n");
+	return;
+    } 
+}
+void ListDir(char* path)
+{
+    DIR *dp;
+    struct dirent *dirp; 
+    struct stat st;
+    if ((dp = opendir(path)) == NULL)
+    {
+        fprintf(stderr,"can't open directory\n");
+        return;
+    }
+    chdir(path);  
+    while ((dirp = readdir(dp)) != NULL)
+    {
+        if (!(strcmp(dirp->d_name, "."))
+         || !(strcmp(dirp->d_name, ".."))
+         || (dirp->d_name[0] == '.'))
+        {
+            if (aflag == 0)
+            {
+                continue;
+            }   
+        }
+      Print(dirp->d_name); 
+    } 
+    if (closedir(dp))
+    {
+       fprintf(stderr,"can't close the directory \n");
+       return;
+    }
+}
+void Print(char *path)
+{
+    struct stat st;
+    char *fileName = NULL;
+    char time[64];
+    struct passwd *p;
+    if (lstat(path, &st))
+    {
+        fprintf(stderr,"file does not exist.\n");
+        return;
+    }
+    if ((fileName = strrchr(path, '/')) != NULL)
+    {  
+        fileName++;  
+    } 
+    else
+    {
+        fileName = path;
+    }
+    if (lflag == 1)              
+    {
+        (S_ISDIR(st.st_mode)) ? printf("d"): printf("-");
+        (st.st_mode & S_IRUSR) ? printf("r"): printf("-");
+        (st.st_mode & S_IWUSR) ? printf("w"): printf("-");
+        (st.st_mode & S_IXUSR) ? printf("x"): printf("-");
+        (st.st_mode & S_IRGRP) ? printf("r"): printf("-");
+        (st.st_mode & S_IWGRP) ? printf("w"): printf("-");
+        (st.st_mode & S_IXGRP) ? printf("x"): printf("-");
+        (st.st_mode & S_IROTH) ? printf("r"): printf("-");
+        (st.st_mode & S_IWOTH) ? printf("w"): printf("-");
+        (st.st_mode & S_IXOTH) ? printf("x"): printf("-");
+        if ((p = getpwuid(st.st_uid))==NULL)
+	fprintf(stderr,"Can't identify the user name");
+        printf(" %s  ",p->pw_name);
+        printf("%d  ",p->pw_gid);
+        printf("%10d ", st.st_size);
+        strftime(time, sizeof(time), "%Y-%m-%d %H:%M", localtime(&st.st_mtime));
+        printf("%s %s\n",time, fileName);
+    }
+    else
+    {
+        printf("%s\t", fileName);
+    }
+}
+int main(int argc, char* argv[])
+{
+if (argc==1)
+{
+	List(NULL);
+	return 0;
+}
+GetKeys(argc,argv);
+if (optind == argc)
+{
+	List(NULL);
+	return 0;
+}
+for (int i = optind;i<argc;++i)
+{
+  	List(argv[i]);
+}
+return 0;
+}
